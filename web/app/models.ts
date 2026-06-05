c87a54c118c220ee4aed6d6a991fbc32fc539718
Lucas Gonçalves <lucasgoncalves@192.168.100.39>
2026-06-05 14:40:04 -0300

    Atualiza modelos e código

--- web/app/models.ts
+++ web/app/models.ts
@@ -3,330 +3,330 @@ export const models = [
     "name": "Adriano Lima",
     "slug": "adriano-lima",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Afonso Florence",
     "slug": "afonso-florence",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Alice Portugal",
     "slug": "alice-portugal",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Antônio Brito",
     "slug": "antonio-brito",
     "supporter": {
-     "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+     "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Arthur Maia",
     "slug": "arthur-maia",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Bacelar",
     "slug": "bacelar",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Cajado",
     "slug": "cajado",
     "supporter": {
-     "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+     "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Daniel Alencar",
     "slug": "daniel-alencar",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Daniel Almeida",
     "slug": "daniel-almeida",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Deivid Bacelar",
     "slug": "deivid-bacelar",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Duda Sanches",
     "slug": "duda-sanches",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Elisangela",
     "slug": "elisangela",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Gabriel Nunes",
     "slug": "gabriel-nunes",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Ivoneide Caetano",
     "slug": "ivoneide-caetano",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Jaime Vieira Filho",
     "slug": "jaime-vieira-filho",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Jonga",
     "slug": "jonga",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Jorge Solla",
     "slug": "jorge-solla",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Joseildo Ramos",
     "slug": "joseildo-ramos",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Kátia da Saúde",
     "slug": "katia-da-saude",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Lídice da Mata",
     "slug": "lidice-da-mata",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Lucas Reis",
     "slug": "lucas-reis",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Manoel Rocha",
     "slug": "manoel-rocha",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Moema Gramacho",
     "slug": "moema-gramacho",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Neto Carleto",
     "slug": "neto-carleto",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Olívia Santana",
     "slug": "olivia-santana",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Paulo Magalhães",
     "slug": "paulo-magalhaes",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Raimundo Costa",
     "slug": "raimundo-costa",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Ricardo Maia",
     "slug": "ricardo-maia",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Sérgio Brito",
     "slug": "sergio-brito",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Valéria",
     "slug": "valeria",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Valmir Assunção",
     "slug": "valmir-assuncao",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Waldenor",
     "slug": "waldenor",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   },
   {
     "name": "Zé Neto",
     "slug": "ze-neto",
     "supporter": {
-      "width": 500,
-      "height": 600,
-      "left": 650,
-      "top": 790
+      "width": 375,
+      "height": 390,
+      "left": 620,
+      "top": 630
     }
   }
 ] as const;
